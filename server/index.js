c60378bd4d3ab92ed5dca53be804b91fd5d8fca0
Andrea Habib <andrea.atef@yahoo.com>
2021-03-08 07:58:11 -0500

    FIX: fixed issue

--- server/index.js
+++ server/index.js
@@ -10,13 +10,8 @@ const sessionStore = new SequelizeStore({db})
 const PORT = process.env.PORT || 8080
 const app = express()
 const socketio = require('socket.io')
+
 module.exports = app
-
-if (result.error) {
-  throw result.error
-}
-
-console.log(result.parsed)
 
 // This is a global Mocha hook, used for resource cleanup.
 // Otherwise, Mocha v4+ never quits after tests.
